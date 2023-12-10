9bcb8372d7d29bd3c7556fbe92b34da3c8229498
Ethan Eldridge <ejayeldridge@gmail.com>
2023-12-10 15:15:42 -0500

    Implement smashboy, ez
    It's a square. It's rotation is... not real. Unlike
    the bar, where the swing of the bar actually has an
    impact on the rotation. I don't think I've ever
    played a game of tetris where the square's rotation
    point was respected. So. I'll ignore that.

---
+++ core/src/space/peetseater/lewd/mino/pieces/Smashboy.java
@@ -0,0 +1,31 @@
+package space.peetseater.lewd.mino.pieces;
+
+import com.badlogic.gdx.graphics.Color;
+
+// The box
+public class Smashboy extends Mino {
+
+    public Smashboy() {
+        create(Color.YELLOW);
+    }
+
+
+    @Override
+    public void setXY(int x, int y) {
+        // b o
+        // o o
+        b[0].moveOnTo(x, y);
+        b[1].moveRightOf(b[0]);
+        b[2].moveBelow(b[0]);
+        b[3].moveRightOf(b[2]);
+    }
+
+    @Override
+    public void getDirection1() {}
+    @Override
+    public void getDirection2() {}
+    @Override
+    public void getDirection3() {}
+    @Override
+    public void getDirection4() {}
+}
