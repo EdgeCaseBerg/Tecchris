2b744c0a8888b375810f7ba30e43026acb9371b2
Ethan Eldridge <ejayeldridge@gmail.com>
2023-12-10 14:43:45 -0500

    Refactor BlueRicky to use helpers

--- core/src/space/peetseater/lewd/mino/pieces/BlueRicky.java
+++ core/src/space/peetseater/lewd/mino/pieces/BlueRicky.java
@@ -10,80 +10,52 @@ public class BlueRicky extends Mino {
 
     @Override
     public void getDirection1() {
-        int centerX = b[0].x;
-        int centerY = b[0].y;
-
         // Standard backwards L
         //   o
         //   o
         // o o
-        tmpB[0].x = centerX;
-        tmpB[0].y = centerY;
-        tmpB[1].x = centerX;
-        tmpB[1].y = centerY + Block.SIZE;
-        tmpB[2].x = centerX;
-        tmpB[2].y = centerY - Block.SIZE;
-        tmpB[3].x = centerX - Block.SIZE;
-        tmpB[3].y = centerY - Block.SIZE;
+        tmpB[0].moveOnTo(b[0]);
+        tmpB[1].moveAbove(tmpB[0]);
+        tmpB[2].moveBelow(tmpB[0]);
+        tmpB[3].moveLeftOf(tmpB[2]);
         updateXY(1);
     }
 
     @Override
     public void getDirection2() {
-        int centerX = b[0].x;
-        int centerY = b[0].y;
-
         // L on its back
         //  o o o
         //  o
-        tmpB[0].x = centerX;
-        tmpB[0].y = centerY;
-        tmpB[1].x = centerX + Block.SIZE;
-        tmpB[1].y = centerY;
-        tmpB[2].x = centerX - Block.SIZE;
-        tmpB[2].y = centerY;
-        tmpB[3].x = centerX - Block.SIZE;
-        tmpB[3].y = centerY + Block.SIZE;
+        tmpB[0].moveOnTo(b[0]);
+        tmpB[1].moveRightOf(tmpB[0]);
+        tmpB[2].moveLeftOf(tmpB[0]);
+        tmpB[3].moveBelow(tmpB[2]);
         updateXY(2);
     }
 
     @Override
     public void getDirection3() {
-        int centerX = b[0].x;
-        int centerY = b[0].y;
-
         // Standard backwards L flipped vertically
         //   o o
         //   o
         //   o
-        tmpB[0].x = centerX;
-        tmpB[0].y = centerY;
-        tmpB[1].x = centerX;
-        tmpB[1].y = centerY + Block.SIZE;
-        tmpB[2].x = centerX;
-        tmpB[2].y = centerY - Block.SIZE;
-        tmpB[3].x = centerX + Block.SIZE;
-        tmpB[3].y = centerY + Block.SIZE;
+        tmpB[0].moveOnTo(b[0]);
+        tmpB[1].moveBelow(tmpB[0]);
+        tmpB[2].moveAbove(tmpB[0]);
+        tmpB[3].moveRightOf(tmpB[2]);
         updateXY(3);
     }
 
     @Override
     public void getDirection4() {
-        int centerX = b[0].x;
-        int centerY = b[0].y;
-
         // Standard backwards L on its tiptoe
         //
         //   o 0 o
         //       o
-        tmpB[0].x = centerX;
-        tmpB[0].y = centerY;
-        tmpB[1].x = centerX - Block.SIZE;
-        tmpB[1].y = centerY;
-        tmpB[2].x = centerX + Block.SIZE;
-        tmpB[2].y = centerY;
-        tmpB[3].x = centerX + Block.SIZE;
-        tmpB[3].y = centerY - Block.SIZE;
+        tmpB[0].moveOnTo(b[0]);
+        tmpB[1].moveLeftOf(tmpB[0]);
+        tmpB[2].moveRightOf(tmpB[0]);
+        tmpB[3].moveBelow(tmpB[2]);
         updateXY(4);
     }
 
@@ -91,15 +63,10 @@ public class BlueRicky extends Mino {
         //   o <-- b[1]
         //   o <-- b[0] is our center point
         // o o <-- b[2 and 3]
-        b[0].x = x;
-        b[0].y = y;
-        b[1].x = x;
-        b[1].y = y + Block.SIZE;
-        b[2].x = x;
-        b[2].y = y - Block.SIZE;
-        b[3].x = x - Block.SIZE;
-        b[3].y = y - Block.SIZE;
-
-        // TODO tempting to just call getDirection1
+        b[0].moveOnTo(x, y);
+        b[1].moveAbove(b[0]);
+        b[2].moveBelow(b[0]);
+        b[3].moveLeftOf(b[2]);
+        directionToRotate = 1;
     }
 }
