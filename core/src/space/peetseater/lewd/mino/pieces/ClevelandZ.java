1b212f516bbccd3c3512f556b3b83a78a1feabc0
Ethan Eldridge <ejayeldridge@gmail.com>
2023-12-10 15:23:28 -0500

    Implement Cleveland Z piece

---
+++ core/src/space/peetseater/lewd/mino/pieces/ClevelandZ.java
@@ -0,0 +1,68 @@
+package space.peetseater.lewd.mino.pieces;
+
+import com.badlogic.gdx.graphics.Color;
+
+// AKA the left facing Z
+public class ClevelandZ extends Mino{
+    public ClevelandZ() {
+        create(Color.RED);
+    }
+
+
+    @Override
+    public void setXY(int x, int y) {
+        // o o
+        //   b o
+        b[0].moveOnTo(x, y);
+        b[1].moveAbove(b[0]);
+        b[2].moveRightOf(b[0]);
+        b[3].moveLeftOf(b[1]);
+    }
+
+    @Override
+    public void getDirection1() {
+        // o o
+        //   b o
+        tmpB[0].moveOnTo(b[0]);
+        tmpB[1].moveAbove(tmpB[0]);
+        tmpB[2].moveRightOf(tmpB[0]);
+        tmpB[3].moveLeftOf(tmpB[1]);
+        updateXY(1);
+    }
+
+    @Override
+    public void getDirection2() {
+        //     o
+        //   b o
+        //   o
+        tmpB[0].moveOnTo(b[0]);
+        tmpB[1].moveRightOf(tmpB[0]);
+        tmpB[2].moveBelow(tmpB[0]);
+        tmpB[3].moveAbove(tmpB[1]);
+        updateXY(2);
+    }
+
+    @Override
+    public void getDirection3() {
+        //
+        // o b
+        //   o o
+        tmpB[0].moveOnTo(b[0]);
+        tmpB[1].moveBelow(tmpB[0]);
+        tmpB[2].moveLeftOf(tmpB[0]);
+        tmpB[3].moveRightOf(tmpB[1]);
+        updateXY(3);
+    }
+
+    @Override
+    public void getDirection4() {
+        //     o
+        //   o b
+        //   o
+        tmpB[0].moveOnTo(b[0]);
+        tmpB[1].moveLeftOf(tmpB[0]);
+        tmpB[2].moveAbove(tmpB[0]);
+        tmpB[3].moveBelow(tmpB[1]);
+        updateXY(4);
+    }
+}
