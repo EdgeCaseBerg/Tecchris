b731db9bc54fc9b61740c774369c323a95ca566d
Ethan Eldridge <ejayeldridge@gmail.com>
2023-12-28 15:39:23 -0500

    Make HoldableKey not an inner class

--- core/src/space/peetseater/picture/mino/inputs/KeyboardInput.java
+++ core/src/space/peetseater/picture/mino/inputs/KeyboardInput.java
@@ -4,33 +4,6 @@ import com.badlogic.gdx.InputAdapter;
 
 public class KeyboardInput extends InputAdapter {
 
-
-
-    class HoldableKey {
-        public boolean pressed = false;
-        public float secondsHeld = 0.0f;
-        public boolean beginCounting = false;
-
-        public HoldableKey() {}
-        public void update(float seconds) {
-            if (beginCounting) {
-                secondsHeld += seconds;
-            }
-        }
-        public void reEnableAfter(float threshold) {
-            if (secondsHeld >= threshold) {
-                pressed = true;
-                secondsHeld = 0.0f;
-            }
-        }
-
-        public void reset() {
-            pressed = false;
-            secondsHeld = 0.0f;
-            beginCounting = false;
-        }
-    }
-
     private boolean downPressed, pausePressed = false;
     private boolean quitPressed = false;
     private boolean ghostDisabled = false;
